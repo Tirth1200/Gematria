24ade052a8260e6d68e3a953f9917730a51edb5b
visualstudio <visualstudio@122-PC25>
2019-04-12 18:15:55 -0400

    working code

--- Gematri/Program.cs
+++ Gematri/Program.cs
@@ -8,8 +8,33 @@ namespace Gematri
 {
     class Program
     {
-        static void Main(string[] args)
+
+            string[] Letters = new string[26] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "1",
+"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        
+        public static void Main()
         {
+
+            Program orange = new Program();
+            Console.WriteLine(orange.Gematria("universe"));
+            Console.ReadLine();
+        }
+
+    public int Gematria(string word)
+        { 
+        
+            int GematriaValue = 0;
+            for (int i = 0; i < word.Length; i++)
+            { GematriaValue += LetterValue(word[i].ToString()); }
+            return GematriaValue;
+        }
+
+        public int LetterValue(string letter) {
+            int x=0;
+            while (Letters[x] != letter) {
+                if (Letters[x++] == letter) return -1;
+            }
+            return x + 1;
         }
     }
 }
